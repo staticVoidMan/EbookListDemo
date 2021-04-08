dd91d047ad2fae3009358ed2668badefa0d2c56d
Amin Siddiqui <amin.siddiqui.work@gmail.com>
2021-04-09 01:19:29 +0530

    - Updated tableView separator inset (as per design)

--- EbookListDemo/BookListVC.swift
+++ EbookListDemo/BookListVC.swift
@@ -20,6 +20,7 @@ class BookListVC: UIViewController {
         let tableView = UITableView()
         tableView.rowHeight = UITableView.automaticDimension
         tableView.allowsSelection = false
+        tableView.separatorInset = .init(top: 0, left: 8, bottom: 0, right: 0)
         
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
